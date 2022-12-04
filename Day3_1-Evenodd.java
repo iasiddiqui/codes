d4d0ed7270666ce1f942126f33d862e24718513d
Ishan Ahmad Siddiqui <115731094+iasiddiqui@users.noreply.github.com>
2022-12-04 17:35:06 +0530

    Create Day3_1-Evenodd.java

---
+++ Day3_1-Evenodd.java
@@ -0,0 +1,21 @@
+//User function Template for Java
+
+
+// Function to check if number is even or odd
+class Geeks {
+    // 
+	 static void evenOdd (int a,int b) 
+	 {
+		//Your code here
+		if(a%2==0) {
+		    System.out.println(a);
+		    System.out.println(b);
+		}
+		else {
+		    System.out.println(b);
+		    System.out.println(a);
+		}
+	     
+	 }
+}
+
