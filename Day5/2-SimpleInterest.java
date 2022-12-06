42feeeaf7885ca239d904576c212528d9829fa64
Ishan Ahmad Siddiqui <115731094+iasiddiqui@users.noreply.github.com>
2022-12-06 16:52:30 +0530

    Create 2-SimpleInterest.java

---
+++ Day5/2-SimpleInterest.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void find() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter price");
+        int p = sc.nextInt();
+        System.out.println("enter rate of interest");
+        int r = sc.nextInt();
+        System.out.println("enter time");
+        int t = sc.nextInt();
+        int SI = (p * r * t) / 100;
+        System.out.println("Simple intereset=" + SI);
+    }
+
+    public static void main(String[] args) {
+        find();
+    }
+
+}
