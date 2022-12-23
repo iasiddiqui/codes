16def41419617cf39edb1e6039a492b2d951bff8
Ishan Ahmad Siddiqui <115731094+iasiddiqui@users.noreply.github.com>
2022-12-23 21:44:16 +0530

    Create 1- TypeCasting.java

---
+++ Day22/1- TypeCasting.java
@@ -0,0 +1,23 @@
+//Widening Casting
+
+public class TypeCasting {
+  public static void main(String[] args) {
+    int myInt = 9;
+    double myDouble = myInt; // Automatic casting: int to double
+
+    System.out.println(myInt);
+    System.out.println(myDouble);
+  }
+}
+
+//Narrowing Casting
+
+public class TypeCast {
+  public static void main(String[] args) {
+    double myDouble = 9.78d;
+    int myInt = (int) myDouble; // Manual casting: double to int
+
+    System.out.println(myDouble);   // Outputs 9.78
+    System.out.println(myInt);      // Outputs 9
+  }
+}
