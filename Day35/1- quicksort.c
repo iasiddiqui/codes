4e0a3d9fa7db2293d2e3ee3e4ac2ed0ca354dc8d
Ishan Ahmad Siddiqui <115731094+iasiddiqui@users.noreply.github.com>
2023-01-05 16:29:45 +0530

    Update 1- quicksort.c

--- Day35/1- quicksort.c
+++ Day35/1- quicksort.c
@@ -1,3 +1,8 @@
+/*Sort a given set of elements using the quick sort method and determine the time required to sort
+the elements. Repeat the experiment for different values of n, the number of elements in the 1st to
+be sorted and plot a graph of the time taken versus n. The elements can be read from a file or can
+be generated using the random number generator.*/
+
 #include<stdio.h>
 #include<sys/time.h>
 void swap(int *x,int *y)
